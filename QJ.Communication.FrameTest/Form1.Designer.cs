eb8e086c2f4e226cad2e1812778fd7274a25bd69
chadigogod <asd281194533@gmail.com>
2025-01-24 17:31:46 +0800

    Update Demo file

--- QJ.Communication.FrameTest/Form1.Designer.cs
+++ QJ.Communication.FrameTest/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "寫測試";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(120, 78);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
--- QJ.Communication.FrameTest/Form1.cs
+++ QJ.Communication.FrameTest/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using QJ.Communication.Core.Cores.Tcp;
+using QJ.Communication.Core.Model.Result;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -35,7 +36,7 @@ namespace QJ.Communication.FrameTest
             var device = new TcpCore("ModbusTcpV2", out bool _Init);
             // 設備參數
             device.IpAddress = "127.0.0.1";
-            device.Port = 502;       
+            device.Port = 502;
             // 顯示請求封包
             device.GetPluginBase().IsShowRequestMessage = true;
 
@@ -62,10 +63,90 @@ namespace QJ.Communication.FrameTest
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            // 獲取設備通訊插件本體
             var plugin = _TcpcDevices["設備1號"].GetPluginBase();
-            var res = await plugin.WriteAsync("0x", 0, new bool[2] { false, false });
-            if (!res.IsOk) Console.WriteLine(res.Message);
+            var res = new QJResult();
+            var addrStr = "4x";
+            var addrStr2 = "0x";
+#if true
+
+            res = plugin.Write(addrStr2, 100, true);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr2, 0, new bool[16] { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 5, (ushort)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 6, new ushort[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 10, (short)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 11, new short[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 20, (uint)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 22, new uint[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 30, (int)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 32, new int[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+
+            res = plugin.Write(addrStr, 100, (ulong)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 104, new ulong[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+
+            res = plugin.Write(addrStr, 120, (long)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 124, new long[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 140, (float)123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 144, new float[2] { 123.123f, 456.456f });
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 160, (double)123.123);
+            Console.WriteLine(res.Message);
+            res = plugin.Write(addrStr, 164, new double[2] { 123.123, 456.456 });
+            Console.WriteLine(res.Message);
+#else 
+            res = await plugin.WriteAsync("0x", 17, true);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync("0x", 20, new bool[12] { true ,true , false , true, false, true, false, true, false, true, false, true });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 5, (ushort)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 6, new ushort[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 10, (short)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 11, new short[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 20, (uint)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 22, new uint[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 30, (int)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 32, new int[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+
+            res = await plugin.WriteAsync(addrStr, 100, (ulong)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 104, new ulong[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+
+            res = await plugin.WriteAsync(addrStr, 120, (long)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 124, new long[2] { 123, 456 });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 140, (float)123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 144, new float[2] { 123.123f, 456.456f });
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 160, (double)123.123);
+            Console.WriteLine(res.Message);
+            res = await plugin.WriteAsync(addrStr, 164, new double[2] { 123.123, 456.456 });
+            Console.WriteLine(res.Message);
+#endif
         }
     }
 }
