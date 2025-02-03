53b31d7de2a5f5a14d666f222916ccea3438f27f
chadigogod <asd281194533@gmail.com>
2025-02-03 18:24:42 +0800

    Update text

--- QJ.Communication.Demo2/Form1.Designer.cs
+++ QJ.Communication.Demo2/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 101);
             this.button1.TabIndex = 0;
+            this.button1.Text = "測試";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
