6079f3ee3c94f92172350e415d10502022157225
godchadigo <asd281194533@gmail.com>
2026-01-13 00:06:01 +0800

    更新事件綁定Demo

---
+++ QJ.Communication.Study.SubEvent/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace QJ.Communication.Study.SubEvent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.disconnect_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ip_tbox = new System.Windows.Forms.TextBox();
+            this.port_tbox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.read_btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.connect_btn);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.disconnect_btn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ip_tbox);
+            this.groupBox1.Controls.Add(this.port_tbox);
+            this.groupBox1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(451, 221);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "連線配置";
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.connect_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.connect_btn.Location = new System.Drawing.Point(55, 128);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(156, 65);
+            this.connect_btn.TabIndex = 1;
+            this.connect_btn.Text = "連接";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(295, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Port";
+            // 
+            // disconnect_btn
+            // 
+            this.disconnect_btn.Font = new System.Drawing.Font("新細明體", 16F);
+            this.disconnect_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.disconnect_btn.Location = new System.Drawing.Point(218, 128);
+            this.disconnect_btn.Name = "disconnect_btn";
+            this.disconnect_btn.Size = new System.Drawing.Size(156, 65);
+            this.disconnect_btn.TabIndex = 2;
+            this.disconnect_btn.Text = "斷開";
+            this.disconnect_btn.UseVisualStyleBackColor = true;
+            this.disconnect_btn.Click += new System.EventHandler(this.disconnect_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(140, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP";
+            // 
+            // ip_tbox
+            // 
+            this.ip_tbox.Location = new System.Drawing.Point(55, 76);
+            this.ip_tbox.Name = "ip_tbox";
+            this.ip_tbox.Size = new System.Drawing.Size(224, 46);
+            this.ip_tbox.TabIndex = 3;
+            this.ip_tbox.Text = "127.0.0.1";
+            // 
+            // port_tbox
+            // 
+            this.port_tbox.Location = new System.Drawing.Point(285, 76);
+            this.port_tbox.Name = "port_tbox";
+            this.port_tbox.Size = new System.Drawing.Size(89, 46);
+            this.port_tbox.TabIndex = 4;
+            this.port_tbox.Text = "502";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 248);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1002, 292);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.read_btn);
+            this.groupBox2.Location = new System.Drawing.Point(478, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(545, 213);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // read_btn
+            // 
+            this.read_btn.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.read_btn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.read_btn.Location = new System.Drawing.Point(65, 74);
+            this.read_btn.Name = "read_btn";
+            this.read_btn.Size = new System.Drawing.Size(414, 65);
+            this.read_btn.TabIndex = 8;
+            this.read_btn.Text = "讀取4x0 10筆數據";
+            this.read_btn.UseVisualStyleBackColor = true;
+            this.read_btn.Click += new System.EventHandler(this.read_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1035, 570);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "事件訂閱教學";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button disconnect_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ip_tbox;
+        private System.Windows.Forms.TextBox port_tbox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button read_btn;
+    }
+}
+
---
+++ QJ.Communication.Study.SubEvent/Form1.cs
@@ -0,0 +1,213 @@
+﻿using QJ.Communication.Core.Cores;
+using QJ.Communication.Core.Cores.Tcp;
+using QJ.Communication.Core.Extension;
+using QJ.Communication.Core.Interface;
+using QJ.Communication.Core.Model.Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QJ.Communication.Study.SubEvent
+{
+    public partial class Form1 : Form
+    {
+        private QJManager _QJManager;
+        private TcpCore _tcpCore;
+        private bool _isReady = false;
+        private CancellationTokenSource _autoRefreshCTS;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 第一步 初始化核心
+            InitQJCommunication();
+
+            // 第二步 使用TcpCore實例化插件
+            _tcpCore = new TcpCore("ModbusTcpV2", out bool initResult);
+            _isReady = initResult;
+            if (!_isReady) MessageBox.Show("插件初始化失敗，請檢察插件是否正確載入!");
+
+            // 綁定事件
+            SubEvent(_tcpCore.qJPlugin);
+
+            // 初始化表格
+            dataGridView1.GridInit();
+        }
+
+        #region QJCommunication
+        private void InitQJCommunication()
+        {
+            _QJManager = new QJManager();
+            _QJManager.Init();
+        }
+
+        #endregion
+
+        #region 事件訂閱
+        private void SubEvent(IQJPlugin pluginBase)
+        {
+            #region Tcp插件事件綁定
+            if (pluginBase is QJTcpPluginBase tcpPlugin)
+            {
+                tcpPlugin.OnConnected = async (_plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJTcpPluginBase;
+                    dataGridView1.AddRow("連線事件", $"成功連線到{plugin.IpAddress}:{plugin.Port}", trigDate);
+                };
+
+                tcpPlugin.OnClosed = async (_plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJTcpPluginBase;
+                    dataGridView1.AddRow("斷線事件", $"{plugin.IpAddress}:{plugin.Port} 離線!", trigDate);
+                };
+
+                tcpPlugin.OnDataReceived = async (reciver, _plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJTcpPluginBase;
+                    dataGridView1.AddRow("<<接收事件", $"{reciver.RecivedRawData.ToHexString()}", trigDate);
+                };
+
+                tcpPlugin.OnDataSend = async (data, len, _plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJTcpPluginBase;
+                    dataGridView1.AddRow(">>發送事件", $"{data.ToHexString()}", trigDate);
+                };
+            }
+
+            #endregion
+
+            #region 串口插件事件綁定
+            if (pluginBase is QJSerialPluginBase serialPlugin)
+            {
+                serialPlugin.OnConnected = async (_plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJSerialPluginBase;
+                    dataGridView1.AddRow("連線事件", $"成功連線到{plugin.serialProp.PortName}", trigDate);
+                };
+
+                serialPlugin.OnClosed = async (_plugin, trigDate) =>
+                {
+                    var plugin = _plugin as QJSerialPluginBase;
+                    dataGridView1.AddRow("斷線事件", $"{plugin.serialProp.PortName} 斷開連線", trigDate);
+                };
+            }
+            #endregion
+
+            
+        }
+        #endregion
+
+        #region 連線 斷線控制
+        private void connect_btn_Click(object sender, EventArgs e)
+        {
+            if (!_isReady) MessageBox.Show("插件初始化失敗，請檢察插件是否正確載入!");
+
+            // 第三步 配置TcpCore參數            
+            if (_isReady)
+            {
+                var ip = ip_tbox.Text;
+                var _port = int.TryParse(port_tbox.Text, out int port);
+
+                // 第四步 使用TcpCore進行建立連線
+                var connectTask = Task.Run(() => _tcpCore.ConnectAsync(ip, port, 5000));
+                connectTask.Wait();
+
+                // 輸出通訊結果
+                Console.WriteLine(connectTask.Result.Message);
+
+                Console.WriteLine($"檢查通訊是否成功建立: {_tcpCore.IsOnline}");
+
+            }
+        }
+
+        private void disconnect_btn_Click(object sender, EventArgs e)
+        {
+            if (_tcpCore != null) _tcpCore.DisconnectAsync();
+        }
+        #endregion
+
+        private void read_btn_Click(object sender, EventArgs e)
+        {
+            if (_tcpCore.IsOnline)
+            {
+                Task.Run(() => AutoRefreshData(true));
+            }
+            else
+            {
+                MessageBox.Show($"目前與伺服器離線中!");
+            }
+        }
+        private async Task AutoRefreshData(bool isSingle)
+        {
+            _autoRefreshCTS = new CancellationTokenSource();
+            var token = _autoRefreshCTS.Token;
+            while (!token.IsCancellationRequested)
+            {
+                if (_tcpCore.IsOnline)
+                {
+                    var plugin = _tcpCore.GetPluginBase();
+                    var readResult = plugin.ReadInt16("4x", 0, 10);
+                    if (readResult.IsOk)
+                    {
+                        var values = readResult.Data;
+#if false
+                        for (int i = 0; i < values.Count; i++)
+                        {
+                            UpdateItem($"4x{i}", values[i].ToString());
+                        }
+#endif
+                    }
+                }
+                if (isSingle)
+                {
+                    _autoRefreshCTS.Cancel();
+                    break;
+                }
+                await Task.Delay(1000, token); // 每1秒刷新一次
+            }
+        }
+    }
+
+    public static class GridExtensions
+    {
+        public static void GridInit(this DataGridView grid)
+        {
+            // ** 添加欄位 ** //
+            grid.Columns.Add("事件名稱", "事件名稱");
+            grid.Columns.Add("訊息", "訊息");
+            grid.Columns.Add("發生時間", "發生時間");
+
+
+            // ** 設定表格屬性 ** //
+            grid.AllowUserToDeleteRows = false;                                 //不允許用戶刪除列
+            grid.AllowUserToOrderColumns = false;                               //不允許用戶重新排列
+            grid.AllowUserToResizeColumns = false;                              //不允許用戶改變列寬
+            grid.AllowUserToAddRows = false;                                    //不允許用戶添加列
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;    //自動調整列寬
+            grid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;      //自動調整行寬
+            grid.ReadOnly = true;                                               //只讀模式
+        }
+        public static void AddRow(this DataGridView grid, string eventName, string msg, DateTime date, bool invoke = true)
+        {
+            if (invoke)
+            {
+                grid.BeginInvoke(new Action(delegate {
+                    grid.Rows.Add(eventName, msg, date.ToString("HH:mm:ss"));
+                }));
+            }
+            else
+            {
+                grid.Rows.Add(eventName, msg, date.ToString("HH:mm:ss"));
+            }
+        }
+
+    }
+}
